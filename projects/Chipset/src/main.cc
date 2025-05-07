56ddc545c4e0b9f092db7a3a1e57e5dd6701e51f
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-05-06 21:35:23 -0700

    added pinout type plus some automatic configuration options

--- projects/Chipset/src/main.cc
+++ projects/Chipset/src/main.cc
@@ -4,19 +4,76 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 #include <RTClib.h>
+#include <type_traits>
 Adafruit_NeoPixel pixel(1, PIN_NEOPIXEL);
 volatile bool hasRTC = false;
 volatile bool hasSDCard = false;
 RTC_DS3231 rtc;
-const char daysOfTheWeek[7][12] {
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+using PinIndex = decltype(PIN_PA02);
+enum class Pin : PinIndex {
+    DAC0 = PIN_PA02,
+    DAC1 = PIN_PA05,
+    READY = PIN_PD12,
+    ADRMUX_DEN1 = PIN_PA12,
+    ADRMUX_DEN2 = PIN_PA13,
+    ADRMUX_CLK = PIN_PA14,
+    // Alternate view for the pinout
+    ADRMUX_SEL0 = ADRMUX_DEN1,
+    ADRMUX_SEL1 = ADRMUX_DEN2,
+    ADRMUX_EN = ADRMUX_CLK,
+    BLAST = PIN_PA15,
+    ADRMUX0 = PIN_PA16,
+    ADRMUX1 = PIN_PA17,
+    ADRMUX2 = PIN_PA18,
+    ADRMUX3 = PIN_PA19,
+    ADRMUX4 = PIN_PA20,
+    ADRMUX5 = PIN_PA21,
+    ADRMUX6 = PIN_PA22,
+    ADRMUX7 = PIN_PA23,
+    BE0 = PIN_PB14,
+    BE1 = PIN_PB15,
+    WR = PIN_PB19,
+    Data0 = PIN_PC00,
+    Data1 = PIN_PC01,
+    Data2 = PIN_PC02,
+    Data3 = PIN_PC03,
+    Data4 = PIN_PC04,
+    Data5 = PIN_PC05,
+    Data6 = PIN_PC06,
+    Data7 = PIN_PC07,
+    Data8 = PIN_PC10,
+    Data9 = PIN_PC11,
+    Data10 = PIN_PC12,
+    Data11 = PIN_PC13,
+    Data12 = PIN_PC14,
+    Data13 = PIN_PC15,
+    Data14 = PIN_PC16,
+    Data15 = PIN_PC17,
 };
+
+[[gnu::always_inline]]
+inline void 
+pinMode(Pin targetPin, decltype(OUTPUT) direction) noexcept {
+    ::pinMode(static_cast<std::underlying_type_t<Pin>>(targetPin), direction);
+}
+
+[[gnu::always_inline]]
+inline void
+digitalWrite(Pin targetPin, decltype(LOW) value) noexcept {
+    ::digitalWrite(static_cast<std::underlying_type_t<Pin>>(targetPin), value);
+}
+
+[[gnu::always_inline]]
+inline auto
+digitalRead(Pin targetPin) noexcept {
+    return ::digitalRead(static_cast<std::underlying_type_t<Pin>>(targetPin));
+}
+[[gnu::always_inline]]
+inline void
+outputPin(Pin targetPin, decltype(LOW) initialValue = HIGH) noexcept {
+    pinMode(targetPin, OUTPUT);
+    digitalWrite(targetPin, initialValue);
+}
 void
 setupRTC() noexcept {
     if (!rtc.begin()) {
@@ -31,42 +88,26 @@ setupRTC() noexcept {
     }
 }
 void
-setup() {
-    Serial.begin(9600);
-    SD.begin();
-    SPI.begin();
+setupNeopixel() noexcept {
+    // enable the onboard neopixel
     pixel.begin();
     pixel.setBrightness(1);
 }
 void
-displayDateTime() noexcept {
-    if (hasRTC) {
-        auto now = rtc.now();
-        Serial.print(now.year(), DEC);
-        Serial.print('/');
-        Serial.print(now.month(), DEC);
-        Serial.print('/');
-        Serial.print(now.day(), DEC);
-        Serial.print(" (");
-        Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-        Serial.print(") ");
-        Serial.print(now.hour(), DEC);
-        Serial.print(':');
-        Serial.print(now.minute(), DEC);
-        Serial.print(':');
-        Serial.print(now.second(), DEC);
-        Serial.println();
-
-        Serial.print(" since midnight 1/1/1970 = ");
-        Serial.print(now.unixtime());
-        Serial.print("s = ");
-        Serial.print(now.unixtime() / 86400L);
-        Serial.println("d");
-    }
+setup() {
+    Serial.begin(9600);
+    SD.begin();
+    SPI.begin();
+    Wire.begin();
+    outputPin(Pin::READY, HIGH);
+    outputPin(Pin::ADRMUX_SEL0, LOW);
+    outputPin(Pin::ADRMUX_SEL1, LOW);
+    outputPin(Pin::ADRMUX_EN, HIGH);
+    setupRTC();
+    setupNeopixel();
 }
 void
 loop() {
-    //displayDateTime();
     pixel.clear();
     pixel.setPixelColor(0, pixel.Color(random(), random(), random()));
     pixel.show();
