24cf2ccfd4809096ed96a0f64a73d969746657e9
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-05-06 20:21:44 -0700

    Neopixel inclusion

--- projects/Chipset/src/main.cc
+++ projects/Chipset/src/main.cc
@@ -2,15 +2,22 @@
 #include <SD.h>
 #include <SPI.h>
 #include <Wire.h>
-
+#include <Adafruit_NeoPixel.h>
+Adafruit_NeoPixel pixel(1, PIN_NEOPIXEL);
 void
 setup() {
+    Serial.begin(9600);
     SD.begin();
     SPI.begin();
     Wire.begin();
+    pixel.begin();
+    pixel.setBrightness(1);
 }
 
 void
 loop() {
-
+    pixel.clear();
+    pixel.setPixelColor(0, pixel.Color(random(), random(), random()));
+    pixel.show();
+    delay(500);
 }
