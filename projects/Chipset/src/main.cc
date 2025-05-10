723a7da70d841bc5f3036a3031bf8689ff1533ae
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-05-10 14:48:23 -0700

    seed random using the analog pins before they are reassigned

--- projects/Chipset/src/main.cc
+++ projects/Chipset/src/main.cc
@@ -324,7 +324,16 @@ setupSerialConsole() noexcept {
     setNeopixelColor(0, 0, 0);
 }
 void
+seedRandom() noexcept {
+    long newSeed = 0l;
+    for (auto a : {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15}) {
+        newSeed += analogRead(a);
+    }
+    randomSeed(newSeed);
+}
+void
 setup() {
+    seedRandom();
     setupNeopixel();
     setupSerialConsole();
     configurePins();
