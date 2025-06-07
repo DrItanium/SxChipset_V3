a6dcbcf9a9f9560299d1c666981d0389f267e5f3
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-06-07 12:14:45 -0700

    Move the Pin type out to a separate header

--- projects/Chipset/src/main.cc
+++ projects/Chipset/src/main.cc
@@ -1,3 +1,27 @@
+/*
+SxChipset_v3
+Copyright (c) 2025, Joshua Scoggins
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include <Arduino.h>
 #include <type_traits>
 #include <concepts>
@@ -14,6 +38,8 @@
 #include <RTClib.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1351.h>
+#include <Adafruit_EEPROM_I2C.h>
+#include <Pinout.h>
 // Thanks to an interactive session with copilot I am realizing that while the
 // CH351 has some real limitations when it comes to write operations. There are
 // minimum hold times in between writes. Which is around 50 ns
@@ -24,6 +50,25 @@ constexpr auto OLEDScreenHeight = 128;
 constexpr uint32_t OnboardSRAMCacheSize = 2048;
 constexpr auto MemoryPoolSizeInBytes = (16 * 1024 * 1024);  // 16 megabyte psram pool
 RTC_DS3231 rtc;
+Adafruit_SSD1351 tft(OLEDScreenWidth, 
+        OLEDScreenHeight, 
+        &SPI,
+        pinIndexConvert(Pin::EYESPI_TCS),
+        pinIndexConvert(Pin::EYESPI_DC),
+        pinIndexConvert(Pin::EYESPI_RST));
+Metro screenUpdate{1};
+constexpr uint16_t color565(uint8_t r, uint8_t g, uint8_t b) noexcept {
+    // taken from the color565 routine in Adafruit GFX
+    return (static_cast<uint16_t>(r & 0xF8) << 8) |
+           (static_cast<uint16_t>(g & 0xFC) << 3) |
+           (static_cast<uint16_t>(b >> 3));
+}
+constexpr uint16_t Color_Black = color565(0, 0, 0);
+constexpr uint16_t Color_Blue = color565(0, 0, 255);
+constexpr uint16_t Color_Red = color565(255, 0, 0);
+constexpr uint16_t Color_Green = color565(0, 255, 0);
+constexpr uint16_t Color_Purple = color565(255, 0, 255);
+constexpr uint16_t Color_White = color565(255, 255, 255);
 template<typename T>
 struct TreatAs final {
     using underlying_type = T;
@@ -247,125 +292,6 @@ RandomSourceRelatedThings randomSource;
 EXTMEM MemoryCellBlock memory960[MemoryPoolSizeInBytes / sizeof(MemoryCellBlock)];
 EEPROMWrapper eeprom{0};
 RTCMemoryBlock rtcInterface;
-enum class Pin : uint8_t {
-  RPI_D0 = 19,
-  RPI_D1 = 18,
-  RPI_D2 = 14,
-  RPI_D3 = 15,
-  RPI_D4 = 40,
-  RPI_D5 = 41,
-  RPI_D6 = 17,
-  RPI_D7 = 16,
-  RPI_D8 = 22,
-  RPI_D9 = 23,
-  RPI_D10 = 20,
-  RPI_D11 = 21,
-  RPI_D12 = 38,
-  RPI_D13 = 39,
-  RPI_D14 = 26,
-  RPI_D15 = 27,
-  RPI_D16 = 8,
-  RPI_D17 = 7,
-  RPI_D18 = 36,
-  RPI_D19 = 37,
-  RPI_D20 = 32,
-  RPI_D21 = 9,
-  RPI_D22 = 6,
-  RPI_D23 = 2,
-  RPI_D24 = 3,
-  RPI_D25 = 4,
-  RPI_D26 = 33,
-  RPI_D27 = 5,
-  RPI_D28 = 28,
-  RPI_D29 = 29,
-  RPI_D30 = 30,
-  RPI_D31 = 31,
-  RPI_D32 = 34,
-  RPI_D33 = 35,
-  EBI_A5 = RPI_D0, // AD_B0_03
-  EBI_A4 = RPI_D1, // AD_B0_02
-  EBI_A3 = RPI_D2, // EMC_04
-  EBI_A2 = RPI_D3, // EMC_05
-  EBI_A1 = RPI_D4, // EMC_06
-  EBI_A0 = RPI_D5, // EMC_08
-  EBI_RD = RPI_D6, // B0_10
-  EBI_WR = RPI_D7, // B1_01
-  EBI_D0 = RPI_D8, // B1_00
-  EBI_D1 = RPI_D9, // B0_11
-  EBI_D2 = RPI_D10, // B0_00
-  EBI_D3 = RPI_D11, // B0_02
-  EBI_D4 = RPI_D12, // B0_01
-  EBI_D5 = RPI_D13, // B0_03
-  EBI_D6 = RPI_D14, // AD_B1_02
-  EBI_D7 = RPI_D15, // AD_B1_03
-  ADS = RPI_D16,
-  BLAST = RPI_D17,
-  DEN = RPI_D18,
-  WR = RPI_D19,
-  BE0 = RPI_D20,
-  BE1 = RPI_D21,
-  LOCK = RPI_D22,
-  HOLD = RPI_D23,
-  INT960_3 = RPI_D24,
-  INT960_2 = RPI_D25,
-  INT960_1 = RPI_D26,
-  INT960_0 = RPI_D27,  
-  RESET = RPI_D28,
-  HLDA = RPI_D29,
-  READY = RPI_D30,
-  READY_SYNC = RPI_D31,
-  EYESPI_TCS = 10,
-  EYESPI_RST = 1,
-  EYESPI_DC = 0,
-};
-
-constexpr std::underlying_type_t<Pin> pinIndexConvert(Pin value) noexcept {
-  return static_cast<std::underlying_type_t<Pin>>(value);
-}
-using PinDirection = decltype(OUTPUT);
-using PinValue = decltype(HIGH);
-inline void pinMode(Pin pin, PinDirection direction) noexcept {
-  ::pinMode(pinIndexConvert(pin), direction);
-}
-
-inline void digitalWriteFast(Pin pin, PinValue value) noexcept {
-  ::digitalWriteFast(pinIndexConvert(pin), value);
-}
-inline void digitalWrite(Pin pin, PinValue value) noexcept {
-  ::digitalWrite(pinIndexConvert(pin), value);
-}
-inline decltype(auto) digitalReadFast(Pin pin) noexcept {
-  return ::digitalReadFast(pinIndexConvert(pin));
-}
-inline decltype(auto) digitalRead(Pin pin) noexcept {
-  return ::digitalRead(pinIndexConvert(pin));
-}
-inline void attachInterrupt(Pin pin, void (*function)(void), decltype(RISING) mode) noexcept {
-  ::attachInterrupt(digitalPinToInterrupt(pinIndexConvert(pin)), function, mode);
-}
-
-inline void digitalToggle(Pin pin) noexcept {
-  ::digitalToggle(pinIndexConvert(pin));
-}
-template<Pin p>
-inline void digitalToggle() noexcept {
-  digitalToggle(p);
-}
-
-inline void digitalToggleFast(Pin pin) noexcept {
-  ::digitalToggleFast(pinIndexConvert(pin));
-}
-template<Pin p>
-inline void digitalToggleFast() noexcept {
-  digitalToggleFast(p);
-}
-inline void outputPin(Pin p, PinValue initialValue) noexcept {
-  pinMode(p, OUTPUT);
-  digitalWrite(p, initialValue);
-}
-inline void inputPin(Pin p) noexcept {
-  pinMode(p, INPUT);
-}
 volatile bool adsTriggered = false;
 volatile bool readyTriggered = false;
 struct CH351 {
@@ -821,25 +747,6 @@ private:
   static inline MemoryCellBlock CLKValues{12 * 1000 * 1000, 6 * 1000 * 1000 };
 };
 
-Adafruit_SSD1351 tft(OLEDScreenWidth, 
-        OLEDScreenHeight, 
-        &SPI,
-        pinIndexConvert(Pin::EYESPI_TCS),
-        pinIndexConvert(Pin::EYESPI_DC),
-        pinIndexConvert(Pin::EYESPI_RST));
-Metro screenUpdate{1};
-constexpr uint16_t color565(uint8_t r, uint8_t g, uint8_t b) noexcept {
-    // taken from the color565 routine in Adafruit GFX
-    return (static_cast<uint16_t>(r & 0xF8) << 8) |
-           (static_cast<uint16_t>(g & 0xFC) << 3) |
-           (static_cast<uint16_t>(b >> 3));
-}
-constexpr uint16_t Color_Black = color565(0, 0, 0);
-constexpr uint16_t Color_Blue = color565(0, 0, 255);
-constexpr uint16_t Color_Red = color565(255, 0, 0);
-constexpr uint16_t Color_Green = color565(0, 255, 0);
-constexpr uint16_t Color_Purple = color565(255, 0, 255);
-constexpr uint16_t Color_White = color565(255, 255, 255);
 void
 setupTFTDisplay() noexcept {
     tft.begin();
