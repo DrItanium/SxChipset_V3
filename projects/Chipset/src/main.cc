cbb11130c0a459605b892458daaff57078ec6331
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-10-18 18:58:37 -0700

    write8 delays down to 80ns total!

--- projects/Chipset/src/main.cc
+++ projects/Chipset/src/main.cc
@@ -485,8 +485,8 @@ static constexpr InterfaceTimingDescription defaultWrite8{
     50, 30, 100, 150
 }; // 330ns worth of delay
 static constexpr InterfaceTimingDescription customWrite8 {
-    30, 0, 30, 30
-}; // 90ns worth of delay
+    30, 0, 30, 20
+}; // 80ns worth of delay
 static constexpr InterfaceTimingDescription defaultRead8 {
     100, 80, 20, 50
 }; // 250ns worth of delay
