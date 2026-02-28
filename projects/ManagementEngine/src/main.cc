8a098f7ee066d4926f48b0f9af6c3531f957e5f7
Joshua Scoggins <Theoretically.x64@gmail.com>
2026-02-27 21:45:59 -0800

    Turn fizzlefade into a single pixel updater routine to allow interleaving with a microshell

--- projects/ManagementEngine/src/main.cc
+++ projects/ManagementEngine/src/main.cc
@@ -367,30 +367,43 @@ setup() {
     chipIsUp = true;
     setupDisplay();
 }
-void
-fizzleFade(int w, int h, uint16_t color) noexcept {
-    uint32_t rndval = 1;
-    do {
-        uint16_t y = rndval & 0x001FF;
-        uint16_t x = (rndval & 0x3FE00) >> 9;
-        unsigned lsb = rndval & 1;
-        rndval >>= 1;
-        if (lsb) {
-            rndval ^= 0x0001'2000;
-        }
-        if (x < w && y < h) {
-            tft.drawPixel(x, y, color);
-        }
-    } while (rndval != 1);
-}
 uint16_t
 randomColor() noexcept {
     uint32_t baseColor = random();
     return tft.color565(static_cast<uint8_t>(baseColor), static_cast<uint8_t>(baseColor >> 8), static_cast<uint8_t>(baseColor >> 16));
 }
+class FizzleFade {
+    public:
+    FizzleFade(int w, int h, uint16_t color = randomColor()) : _width(w), _height(h), _color(color) { }
+    void cycle() noexcept {
+        {
+            uint16_t y = _rndval & 0x001FF;
+            uint16_t x = (_rndval & 0x3FE00) >> 9;
+            unsigned lsb = _rndval & 1;
+            _rndval >>= 1;
+            if (lsb) {
+                _rndval ^= 0x0001'2000;
+            }
+            if (x < _width && y < _height) {
+                tft.drawPixel(x, y, _color);
+            }
+        }
+        if (_rndval == 1) {
+            _rndval = 1;
+            _color = randomColor();
+        }
+    }
+    private:
+        int _width;
+        int _height;
+        uint16_t _color;
+        uint32_t _rndval = 1;
+
+};
+FizzleFade ff(tft.width(), tft.height());
 void
 loop() {
-    fizzleFade(tft.width(), tft.height(), randomColor());
+    ff.cycle();
 //    delay(1000);
 }
 
